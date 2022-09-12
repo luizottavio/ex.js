322d4d58b0c69020a435616b7343ab9dff32e12e
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-12 17:50:11 -0300

    update

--- ex.cronometro/main.js
+++ ex.cronometro/main.js
@@ -1,12 +1,62 @@
-function cronometro() {
+window.onload = function () {
 
-    var primeiros = 00;
-    var dezenas = 00;
-    var acrescentardezens = document.getElementById('dezenas');
-    var acrescentarprimeiros = document.getElementById('primeiros');
-    var buttoniniciar = document.getElementById('button-iniciar');
-    var buttonparar = document.getElementById('button-parar');
-    var buttonrezetar = document.getElementById('button-rezetar');
+  var segundos = 00;
+  var dezenas = 00;
+  var acrescentardezens = document.getElementById('dezenas');
+  var acrescentarsegundos = document.getElementById('segundos');
+  var buttoniniciar = document.getElementById('button-iniciar');
+  var buttonparar = document.getElementById('button-parar');
+  var buttonrezetar = document.getElementById('button-rezetar');
+
+
+
+  buttoniniciar.onclik = function() {
+
+      clearInterval(intervalo);
+      intervalo = setInterval(iniciarTempo, 10);
+  
+  }
+  
+  buttonparar.onclik = function() {
+      clearInterval(intervalo);
+  
+  }
+  
+  buttonrezetar.onclik = function() {
+      clearInterval(intervalo);
+      primeiros = "00";
+      dezenas = "00";
+      appenddezenas.innerHTML = dezenas;
+      appendsegundos.innerHTML = segundos;
+  }
+  
+  function iniciarTempo () {
+      dezenas++;
+  
+      if (dezenas <= 9){
+        appenddezenas.innerHTML = "0" + dezenas;
+      }
+  
+      if (dezenas > 9){
+        appenddezenas.innerHTML = dezenas;
+  
+      }
+  
+      if (dezenas > 99){
+          console.log("segundos");
+          segundos++;
+          appendsegundos.innerHTML = "0" + segundos;
+          dezenas = 0;
+          appenddezenas.innerHTML = "0" + "0";
+      }
+  
+      if (segundos > 9){
+          appendsegundos.innerHTML = segundos;
+      }
+  
+  }
 
 }
+
+
 
