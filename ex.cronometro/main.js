190383c13bbcc7e91e350c76e89410c4d30fe18b
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-12 16:17:10 -0300

    atualizei index.html e main.js

---
+++ ex.cronometro/main.js
@@ -0,0 +1,12 @@
+function cronometro() {
+
+    var primeiros = 00;
+    var dezenas = 00;
+    var acrescentardezens = document.getElementById('dezenas');
+    var acrescentarprimeiros = document.getElementById('primeiros');
+    var buttoniniciar = document.getElementById('button-iniciar');
+    var buttonparar = document.getElementById('button-parar');
+    var buttonrezetar = document.getElementById('button-rezetar');
+
+}
+
