227161256ccf12d8bcde4b3f6d3aae35344cd768
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-13 13:11:57 -0300

    Update in main.js

--- ex.cronometro/main.js
+++ ex.cronometro/main.js
@@ -1,62 +1,49 @@
-window.onload = function () {
+var seconds = 00
+var minutes = 00
+var hours = 00
+var interval
 
-  var segundos = 00;
-  var dezenas = 00;
-  var acrescentardezens = document.getElementById('dezenas');
-  var acrescentarsegundos = document.getElementById('segundos');
-  var buttoniniciar = document.getElementById('button-iniciar');
-  var buttonparar = document.getElementById('button-parar');
-  var buttonrezetar = document.getElementById('button-rezetar');
-
-
-
-  buttoniniciar.onclik = function() {
-
-      clearInterval(intervalo);
-      intervalo = setInterval(iniciarTempo, 10);
-  
+function twodigits(digit) {
+  if (digit < 10) {
+    return('0' + digit);
+  } else {
+    return(digit);
   }
-  
-  buttonparar.onclik = function() {
-      clearInterval(intervalo);
-  
-  }
-  
-  buttonrezetar.onclik = function() {
-      clearInterval(intervalo);
-      primeiros = "00";
-      dezenas = "00";
-      appenddezenas.innerHTML = dezenas;
-      appendsegundos.innerHTML = segundos;
-  }
-  
-  function iniciarTempo () {
-      dezenas++;
-  
-      if (dezenas <= 9){
-        appenddezenas.innerHTML = "0" + dezenas;
-      }
-  
-      if (dezenas > 9){
-        appenddezenas.innerHTML = dezenas;
-  
-      }
-  
-      if (dezenas > 99){
-          console.log("segundos");
-          segundos++;
-          appendsegundos.innerHTML = "0" + segundos;
-          dezenas = 0;
-          appenddezenas.innerHTML = "0" + "0";
-      }
-  
-      if (segundos > 9){
-          appendsegundos.innerHTML = segundos;
-      }
-  
-  }
+}
+
+function start() {
+  timer()
+  interval = setInterval(timer, 1000);
+}
+
+function stop() {
+  clearInterval(interval);
+
+  document.getElementById('timer').innerText = minutes + ':' + seconds;
 
 }
 
 
+function reset() {
+  clearInterval(interval);
+  seconds = 00;
+  minutes = 00;
+  document.getElementById('timer').innerText = '00:00:00'
 
+}
+
+function timer() {
+  seconds++
+
+  if (seconds == 60) {
+    minutes++
+    seconds = 0
+    if (minutes == 60) {
+      minutes = 0
+      hours++
+    }
+  }
+
+  document.getElementById('timer').innerText = twodigits(hours) + ':' + twodigits(minutes) + ':' + twodigits(seconds);
+
+}
