272841568a9a749c9227a81da08257beedc32fec
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-14 11:15:21 -0300

    Udate

--- ex.clock/main.js
+++ ex.clock/main.js
@@ -1,16 +1,27 @@
 function clock() {
 
     var hours = document.getElementById('hours');
-    var minutes = document.getElementById('minutes');
-    var seconds = document.getElementById('secondes');
-
     var newHours = new Date();
     var hor = newHours.getHours();
     var min = newHours.getMinutes();
     var sec = newHours.getSeconds();
+    var interval
 
-    hours,minutes,seconds.innerHTML = `${hor}:${min}:${sec}`;
+    hours.innerHTML = `${hor}:${min}:${sec}`;
+ 
+}
 
-    /*document.getElementById('hours').textContent = hours + ':' + minutes + ':' + seconds;*/
+sec.onload = function () {
+
+    timerSec();
+    interval = setInterval(timerSec, 1000);
 
 }
+
+function timerSec() {
+    sec++
+   
+ 
+}
+
+//document.getSeconds().innerText=sec
